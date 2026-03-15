01d08b439e1f9da31dd66a5e3d918acae20c9018
Mark Tantongco <mark.tantongco@gmail.com>
2026-03-15 14:36:01 +0800

    fix: add missing HeroSection to HomeClient

--- src/app/HomeClient.tsx
+++ src/app/HomeClient.tsx
@@ -2,6 +2,7 @@
 
 import { ScrollProgress } from "@/components/ui/primitives";
 import Navbar from "@/components/sections/Navbar";
+import HeroSection from "@/components/sections/HeroSection";
 import KnowledgeGalaxy from "@/components/sections/KnowledgeGalaxy";
 import PromptBuilder from "@/components/sections/PromptBuilder";
 import SearchSection from "@/components/sections/SearchSection";
@@ -18,6 +19,7 @@ export default function HomeClient() {
     <main>
       <ScrollProgress />
       <Navbar />
+      <HeroSection />
       <KnowledgeGalaxy />
       
       <section id="prompt-builder">
